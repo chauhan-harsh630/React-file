6a0bc4a3144e607119bdbbe36dcbc0543d5cef11
chauhan-harsh630 <hc2668012@gmail.com>
2025-09-13 18:35:32 +0530

    new files

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Greet from './hello.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Greet />
+      <h3>Now  I am call new function </h3>
+    </>
+  );
+}
+
+export default App
